39374be7d80da281d7738d58b85aa6e22f46f5fb
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-03-12 12:52:03 -0500

    Adding extra prompts.

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,12 +1,15 @@
 const { query } = require('express');
 const inquirer = require('inquirer');
 
-const promptOptions = {
-    viewAllDepartments: "View all departments",
-    viewAllRoles: "View all roles",
-    viewAllEmployees: "View all employees",
-    addDepartment: "Add a department",
-    addRole: "Add a role",
+const promptQuestions = {
+    departmentName: "What is the department name?",
+    roleName: "What is the role name?",
+    roleSalary: "What is the salary for this role?",
+    roleDepartment: "What is the role's department id?",
+    employeeFirstName: "What is the employee's first name?",
+    employeeLastName: "What is the employee's last name?",
+    employeeRole: "What is the employee's role id",
+    employeeManager: "What is the employee's manager id (null if none)",
     addEmployee: "Add an employee",
     updateEmployeeRole: "Update an employee role",
     exit: "Exit"
@@ -18,50 +21,49 @@ const initialQuestion = {
     message: 'What would you like to do?',
     loop: false,
     choices: [
-        promptOptions.viewAllDepartments,
-        promptOptions.viewAllRoles,
-        promptOptions.viewAllEmployees,
-        promptOptions.addDepartment,
-        promptOptions.addRole,
-        promptOptions.addEmployee,
-        promptOptions.updateEmployeeRole,
-        promptOptions.exit
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit"
     ]
-
 }
 
 const options = (db) => {
     inquirer.prompt(initialQuestion).then(answer => {
         switch (answer.action) {
-            case promptOptions.viewAllDepartments:
+            case "View all departments":
                 viewAll("departments", db);
                 break;
 
-            case promptOptions.viewAllRoles:
+            case "View all roles":
                 viewAll('roles', db);
                 break;
 
-            case promptOptions.viewAllEmployees:
+            case "View all employees":
                 viewAll("employees", db);
                 break;
 
-            case promptOptions.addDepartment:
+            case "Add a department":
                 add("department", db);
                 break;
 
-            case promptOptions.addRole:
+            case "Add a role":
                 add("role", db);
                 break;
 
-            case promptOptions.addEmployee:
+            case "Add an employee":
                 add("employee", db);
                 break;
 
-            case promptOptions.updateEmployeeRole:
+            case "Update an employee role":
                 updateEmployeeRole(db);
                 break;
 
-            case promptOptions.exit:
+            case "Exit":
                 endSession();
                 break;
         }
@@ -90,11 +92,14 @@ const options = (db) => {
     )
 }
 
-const add = (branch, db, input) => {
+const add = (branch, db) => {
     let query = "";
     switch (branch) {
         case "department":
-            query = `INSERT INTO department (name) VALUES ("?"), (${input});`;
+            const input = namePrompt(promptQuestions.departmentName);
+            console.log(input);
+            console.log("Here");
+            //query = `INSERT INTO department (name) VALUES ("?"), (${input});`;
             break;
         case "role":
             console.log("role.\n");
@@ -104,13 +109,13 @@ const add = (branch, db, input) => {
             console.log("employee.\n");
             break;
     }
-    db.execute(`${query}`,
-        function(err, results) {
-            console.log("\n");
-            console.table(results);
-            (options(db));
-        }
-    )
+    // db.execute(`${query}`,
+    //     function(err, results) {
+    //         console.log("\n");
+    //         console.table(results);
+    //         (options(db));
+    //     }
+    // )
 }
 
 const updateEmployeeRole = db => {
@@ -125,6 +130,25 @@ const endSession = () => {
     console.log("The application has ended.");
     console.log("");
     process.exit();
+}
+
+const namePrompt = (question) => {
+    inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: question,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('You need to enter a name.\n');
+                return false;
+            }
+        }
+    }).then(answer => {
+        console.log("answer:" + answer);
+    });
+    console.log("shut");
 }
 
 module.exports = {options};
